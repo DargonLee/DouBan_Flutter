f0582a2b8661f12562c52d593df5d7cd1940e3ee
DargonLee <2461414445@qq.com>
2020-02-01 20:38:40 +0800

    修复首页顶部间距

--- lib/module/home/home_page.dart
+++ lib/module/home/home_page.dart
@@ -48,7 +48,7 @@ class _HomeState extends State<HomePage> {
         width: AppUtil.screenWidth,
         height: AppUtil.screenHeight - 102,
         child: ListView(
-          padding: EdgeInsets.zero,
+          padding: EdgeInsets.zero ,
           children: <Widget>[
             RecommentItem(
               context: context,
